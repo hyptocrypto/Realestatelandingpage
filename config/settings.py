3d35b37c579e447aa8943b7a6b119b6bc0b68363
Julian baumgartner <julianbaumgartner@Julians-iMac.local>
2020-06-04 14:19:14 -0400

    updated requirements

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k*&nk#047!qjo#lf_^r8hwi-l=q(6v49g^#734)&%qj&6z)0tp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True  
 
 ALLOWED_HOSTS = ['swflreliefrealty.herokuapp.com']
 
@@ -121,9 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
